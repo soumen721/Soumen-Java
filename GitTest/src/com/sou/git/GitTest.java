552bcc4005f7128c040d6738243d6a9c286bd433
Soumen Choudhury <soumenc@rssoftware.co.in>
2016-02-21 19:38:18 +0530

    Adding new file for testing GIT

---
+++ GitTest/src/com/sou/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.sou.git;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git Test");
+	}
+
+}
