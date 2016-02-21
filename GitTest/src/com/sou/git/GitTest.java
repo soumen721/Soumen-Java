25f89b2ee0359a2ccc2ce606182de7417d984147
Soumen Choudhury <soumenc@rssoftware.co.in>
2016-02-21 20:13:25 +0530

    megre Test

--- GitTest/src/com/sou/git/GitTest.java
+++ GitTest/src/com/sou/git/GitTest.java
@@ -4,9 +4,10 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Testing Marge");
 		TestMe tm=new TestMe();
 		String greet=tm.greeting();
-		System.out.println(greet+" Welcome to Git Test");
+		System.out.println(greet+" Welcome to Git Merge Test");
 	}
 
 }
